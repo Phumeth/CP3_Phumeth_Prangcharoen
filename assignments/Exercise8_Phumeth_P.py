a74624e29da2f8834cc863b9273912479b1b16c0
Phumeth <wprangcharoen@gmail.com>
2020-09-09 14:13:57 +0700

    Update Exercise8_Phumeth_P.py
    แก้ไขตัวแปล price และ condition หลังเลือกสินค้าแล้ว

--- assignments/Exercise8_Phumeth_P.py
+++ assignments/Exercise8_Phumeth_P.py
@@ -10,19 +10,27 @@ if userName == "Gundam" and passWord == "destiny":
     print("5. RG Wing Zero      1060 THB")
     userSelected = int(input("Which model do you want? (1-5) : "))
     number = int(input("Number : "))
+    price1 = 1620
+    price2 = 900
+    price3 = 500
+    price4 = 780
+    price5 = 1060
     if userSelected == 1:
-        price = 1620
+        result = price1 * number
+        print("Price =", result, "THB")
     elif userSelected == 2:
-        price = 900
+        result = price2 * number
+        print("Price =", result, "THB")
     elif userSelected == 3:
-        price = 500
+        result = price3 * number
+        print("Price =", result, "THB")
     elif userSelected == 4:
-        price = 780
+        result = price4 * number
+        print("Price =", result, "THB")
     elif userSelected == 5:
-        price = 1060
+        result = price5 * number
+        print("Print =", result, "THB")
     else:
-        print("Incorrect model choose")
-    result = number * price
-    print("Price = ", result, "THB")
+        print("Wrong number")
 else:
     print("Username or password incorrect")
